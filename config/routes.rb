f12c4e87138485d12f3f7aa7111aca6c893ef9e4
Booneteam <boone.garrett@gmail.com>
2013-02-13 23:47:43 -0600

    root

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ FlashCards::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'cards#start'
+   root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
