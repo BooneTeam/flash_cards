03971e58d8a5d0f13ddf87337be82fb5b4bcf173
Booneteam <boone.garrett@gmail.com>
2013-03-06 00:27:32 -0600

    added url mail optins

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ FlashCards::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  # for Heroku
+   config.action_mailer.default_url_options = { :host => 'FlashCardsTest.herokuapp.com' }
   # Mailer Config Gmail
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
